fd0b7b1dc5b5aea6e7fd97044d37b126188e76d5
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-28 19:49:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number]
     @phrase = params[:phrase]
-    @number.to_i.times do 
+    @number.to_i.times do
       "#{@phrase}"
     end
   end
